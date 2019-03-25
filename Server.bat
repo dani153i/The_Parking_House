ef1885874d0bd076898bd5782a29026cae65ed06
dani153i <danielblom90@gmail.com>
2019-03-25 01:08:11 +0100

    update

--- Server.bat
+++ Server.bat
@@ -1,3 +1,7 @@
+@ECHO OFF
+COLOR 0a
+CLS
+
 javac -sourcepath "src" "src\com\tph\server\ServerShell.java" -d "out\production\The_Parking_House\Server"
 PAUSE
 java -Dfile.encoding=CP850 -classpath "out\production\The_Parking_House\Server;lib\mysql-connector-java-8.0.15.jar" com.tph.server.ServerShell
